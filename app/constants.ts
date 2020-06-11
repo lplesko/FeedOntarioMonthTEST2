8b83d09859774c658071f7b7efa05024e50061d3
lplesko <57364521+lplesko@users.noreply.github.com>
2020-06-10 22:21:52 -0400

    Create constants.ts

---
+++ app/constants.ts
@@ -0,0 +1,2 @@
+export const years = [ "2016", "2017", "2018", "2019" ];
+export const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
